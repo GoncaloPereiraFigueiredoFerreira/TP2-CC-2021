c6ebed25a52dbdf43e41b07302591c0be227be4f
Alex-Sa-Ms <a93315@alunos.uminho.pt>
2021-12-06 22:46:14 +0000

    Serialize and deserialize files

--- src/ConnectionWorker.java
+++ src/ConnectionWorker.java
@@ -16,73 +16,124 @@ public class ConnectionWorker extends Thread {
     private final ReentrantLock readLock;
     private final ReentrantLock writeLock;
     private final Map<String,TransferWorker> requestsSent;
-    private final Map<String,TransferWorker> requestsReceived;
+    private final Map<String,TransferWorker> requestsReceived; //Keeps track of the files received and the files that are being received
     private final boolean receiver;
 
-    ConnectionWorker(boolean receiver, String externalIP, String folderPath, Map<String,Long> filesInDir, DatagramSocket ds, ReentrantLock readLock, ReentrantLock writeLock, Map<String,TransferWorker> requestsSent, Map<String,TransferWorker> requestsReceived){
-        this.receiver         = receiver;
-        this.externalIP       = externalIP;
-        this.folderPath       = folderPath;
-        this.filesInDir       = filesInDir;
-        this.ds               = ds;
-        this.ftr              = new FTrapid(ds);
-        this.readLock         = readLock;
-        this.writeLock        = writeLock;
-        this.requestsSent     = requestsSent;
+    ConnectionWorker(boolean receiver, String externalIP, String folderPath, Map<String, Long> filesInDir, DatagramSocket ds, ReentrantLock readLock, ReentrantLock writeLock, Map<String, TransferWorker> requestsSent, Map<String, TransferWorker> requestsReceived) {
+        this.receiver = receiver;
+        this.externalIP = externalIP;
+        this.folderPath = folderPath;
+        this.filesInDir = filesInDir;
+        this.ds = ds;
+        this.ftr = new FTrapid(ds);
+        this.readLock = readLock;
+        this.writeLock = writeLock;
+        this.requestsSent = requestsSent;
         this.requestsReceived = requestsReceived;
     }
 
+    //TODO: Delete file in the receiver client if there is an io error (or something similar) while receiving
+
     @Override
-    public void run(){
+    public void run() {
         try {
             if (receiver) receive();
             else sendWriteRequest();
-        }
-        catch (IOException | OpcodeNotRecognizedException e) { //TODO: Handle exceptions
+        } catch (IOException | OpcodeNotRecognizedException e) { //TODO: Handle exceptions
             e.printStackTrace();
         }
     }
 
-    //TODO: Delete file in the receiver client if there is an error while receiving
-    //TODO: Remove from map files that already exist(or were modified recently) in the order computer
-    //returns false if we already own that file in a more recent version
-    public boolean analyse(RequestPackageInfo rq){
-        String filename = rq.getFilename();
-        boolean ret = true;
-        if (this.filesInDir.containsKey(filename)){
-            ret = this.filesInDir.get(filename) < rq.getData();
-            if (ret) this.filesInDir.remove(filename);
-        }
-        return ret;
-    }
-
     public void sendWriteRequest() throws IOException, OpcodeNotRecognizedException {
-        Set<String> rq = this.filesInDir.keySet();
+        String filename; Long date;
+        Map.Entry<String,Long> entry;
+        Iterator<Map.Entry<String,Long>> it = this.filesInDir.entrySet().iterator();
+
         DatagramSocket datagramSocket = null;
         short port;
 
-        for (String filepath : rq){
+        while(it.hasNext()) {
+            entry = it.next(); filename = entry.getKey(); date = entry.getValue();
+
             //Gets local usable port
             datagramSocket = createDatagramSocket();
-            port = (short) datagramSocket.getLocalPort(); System.out.println("Vou escrever por esta porta: " + port);//(PRINT)
-
-            if(datagramSocket != null){
-                try {
-                    writeLock.lock();
-                    ftr.requestRRWR(filepath, port, (short) 2, this.filesInDir.get(filepath));
-                } finally { writeLock.unlock(); }
-                TransferWorker tw = new TransferWorker(true, false, folderPath, filepath, datagramSocket);
-                requestsSent.put(filepath, tw);
+            while (datagramSocket == null) {
+                //Sleeps 1 second and tries to get a valid socket again
+                try { sleep(1000); } catch (InterruptedException ignored) {}
+                datagramSocket = createDatagramSocket();
             }
+            port = (short) datagramSocket.getLocalPort();
+            System.out.println("Vou escrever por esta porta: " + port);//(PRINT)
+
+            try {
+                writeLock.lock();
+                ftr.requestRRWR(filename, port, (short) 2, date);
+                //filesInDir.remove(filename); //Removes from "queue"
+            } finally { writeLock.unlock(); }
+
+            TransferWorker tw = new TransferWorker(true, false, folderPath, filename, datagramSocket);
+            requestsSent.put(filename, tw);
+
+            it.remove(); //Removes from the "queue"
         }
     }
 
-    public void receiveWriteRequest(DatagramPacket dp) throws OpcodeNotRecognizedException, IOException {
-        DatagramSocket dsTransferWorker = null;
-        RequestPackageInfo rpi          = ftr.analyseRequest(dp);
-        String filename                 = rpi.getFilename();
+    //TODO: Se receber erro "Erro ao receber ficheiro", voltar a adicionar ao map de ficheiros pertencentes à pasta
+    //TODO: add locks to the data structures
+    public void receive() throws IOException, OpcodeNotRecognizedException {
+        DatagramPacket dp = new DatagramPacket(new byte[FTrapid.MAXRDWRSIZE], FTrapid.MAXRDWRSIZE);
+        boolean flag = true;
 
-        //New Request
+        while (flag) {
+            try {
+                readLock.lock();
+                ds.receive(dp);
+            } finally { readLock.unlock(); }
+
+            if (ftr.verifyPackage(dp.getData()) == FTrapid.WRopcode) {
+                try {
+                    receiveWriteRequest(dp);
+                } catch (IntegrityException e) {
+                    e.printStackTrace();
+                }
+            }
+            else if (ftr.verifyPackage(dp.getData()) == FTrapid.SYNopcode)
+                receiveSyn(dp);
+            else if (ftr.verifyPackage(dp.getData()) == FTrapid.ERRopcode)
+                receiveError(dp);
+        }
+    }
+
+
+    /* ********** Auxiliar Methods ********** */
+
+    //TODO: Remove from filesInDir files that already exist(or were modified recently) in the other computer
+    //returns true if the client should receive the file
+    public boolean analyseWriteRequest(RequestPackageInfo rq) {
+        boolean ret = true;
+        String filename = rq.getFilename();
+
+        //Checks for the existence of the file. If the file exists, compares the dates when they were last modified.
+        if (this.filesInDir.containsKey(filename)) {
+            ret = this.filesInDir.get(filename) > rq.getData();
+            //if (ret) this.filesInDir.remove(filename); //TODO: find a way that allows the removal of files from the filesInDir. Worst case, we dont remove, and the main threads will have to process more requests
+        }
+        //Verifies the existence of the file in list of requests sent. Compares the dates when they were last modified.
+        else if(this.requestsSent.containsKey(filename)){
+            ret = new File(filename).lastModified() > rq.getData();
+        }
+
+        return ret;
+    }
+
+    //TODO: Verificar se não está a ser enviado o ficheiro q recebeu, caso em q podem a estar a ser enviados ao msm tempo ficheiros com o msm nome
+    private void receiveWriteRequest(DatagramPacket dp) throws OpcodeNotRecognizedException, IOException, IntegrityException {
+        short synOrError, msg;
+        DatagramSocket dsTransferWorker;
+        RequestPackageInfo rpi = ftr.analyseRequest(dp);
+        String filename = rpi.getFilename();
+
+        //New Request. The file is not being received nor was it received
         if (!requestsReceived.containsKey(filename)) {
 
             //Checks for the existence of the file. If the file exists, compares the dates when they were last modified.
@@ -90,123 +141,110 @@ public class ConnectionWorker extends Thread {
                 //TODO: Adicionar sleep caso não hajam threads para responder às necessidades dos requests
                 dsTransferWorker = createDatagramSocket();
                 TransferWorker tw = new TransferWorker(false, true, folderPath, filename, dsTransferWorker);
-                tw.connectToPort(externalIP,rpi.getPort()); System.out.println("Recebi esta porta: " + rpi.getPort());//(PRINT)
+                tw.connectToPort(externalIP, rpi.getPort());
+                System.out.println("Recebi esta porta: " + rpi.getPort());//(PRINT)
                 tw.start();
 
+                //TODO: Provavelmente vai ser necessário adicionar locks para as estruturas de dados
                 requestsReceived.put(filename, tw);
-
-                try {
-                    writeLock.lock();
-                    ftr.answer((short) 2, (short) dsTransferWorker.getLocalPort(), filename);
-                }finally { writeLock.unlock(); }
-            }
-            else{
+                synOrError = 2;
+                msg = (short) dsTransferWorker.getLocalPort();
+            } else {
                 //Rejects the write request if the date of the local file is the latest
-                try {
-                    writeLock.lock();
-                    ftr.answer((short) 1, (short) 401, filename);
-                }finally { writeLock.unlock(); }
+                synOrError = 1;
+                msg = 401;
             }
         } else {
             //Duplicate of a previous request
             //Send SYN package (Resended in case of a DUP Request)
-            try {
-                writeLock.lock();
-                ftr.answer((short) 2, requestsReceived.get(filename).getLocalPort(), filename);
-            }finally { writeLock.unlock(); }
+            synOrError = 2;
+            msg = requestsReceived.get(filename).getLocalPort();
+        }
+
+        //Sends answer
+        try {
+            writeLock.lock();
+            ftr.answer(synOrError, msg, filename);
+        } finally {
+            writeLock.unlock();
         }
     }
 
-    public void receiveSynRequest(DatagramPacket dp) throws OpcodeNotRecognizedException, IOException {
-        ErrorSynPackageInfo espi = ftr.analyseAnswer(dp);
+    private void receiveSyn(DatagramPacket dp) throws OpcodeNotRecognizedException, IOException {
+        ErrorSynPackageInfo espi = null;
+        try {
+            espi = ftr.analyseAnswer(dp);
+        } catch (IntegrityException e) {
+            e.printStackTrace();
+        }
         String filename = espi.getFilename();
         TransferWorker tw;
 
-        //Received DUPLICATE of SYN
         //If there is a transfer worker associated with the file name received, starts it, if it isnt already running
-        if((tw = requestsSent.get(filename)) != null){
-            if(!tw.isAlive()) {
-                tw.connectToPort(externalIP,espi.getMsg());
+        //Receive of SYN duplicate doesnt affect the application
+        if ((tw = requestsSent.get(filename)) != null) {
+            if (!tw.isAlive()) {
+                tw.connectToPort(externalIP, espi.getMsg());
                 tw.start();
             }
         }
         //Sends an error if there isnt a "log" of a thread in charge of sending a file with the name received
-        else{
+        else {
             try {
                 writeLock.lock();
                 ftr.answer((short) 1, (short) 403, filename);
-            }finally { writeLock.unlock(); }
-        }
-    }
-
-    //TODO: add locks to the data structures
-    public void receive() throws IOException, OpcodeNotRecognizedException {
-        DatagramPacket dp = new DatagramPacket(new byte[FTrapid.MAXRDWRSIZE], FTrapid.MAXRDWRSIZE);
-        boolean flag = true;
-        short port = 0;
-
-        while (flag) {
-            try {
-                readLock.lock();
-                ds.receive(dp);
             } finally {
-                readLock.unlock();
-            }
-
-            //Received Write Request
-            if (ftr.verifyPackage(dp.getData()) == FTrapid.WRopcode)
-                receiveWriteRequest(dp);
-            //Received SYN
-            else if (ftr.verifyPackage(dp.getData()) == FTrapid.SYNopcode) {
-
-            }
-            else if(ftr.verifyPackage(dp.getData()) == FTrapid.ERRopcode){
-                ErrorSynPackageInfo espi = ftr.analyseAnswer(dp);
-                short errorCode = espi.getMsg();
-                String filename = espi.getFilename();
-
-                //TODO: Add all the errors
-                //Connection error
-                if(errorCode == 400){
-
-                }
-                //Already owned file or the local file is the latest
-                else if(errorCode == 401){
-                    TransferWorker tw = requestsSent.remove(filename);
-                    tw.closeSocket();
-                }
-                else if(errorCode == 402){
-                    //Remover o ficheiro na maquina que recebe
-                }
-                else if(errorCode == 403){
-                    //TODO: O que fazer aqui?
-                }
+                writeLock.unlock();
             }
         }
     }
 
+    private void receiveError(DatagramPacket dp) {
+        ErrorSynPackageInfo espi = null;
+        try {
+            espi = ftr.analyseAnswer(dp);
+        } catch (IntegrityException e) {
+            e.printStackTrace();
+        }
+        short errorCode = espi.getMsg();
+        String filename = espi.getFilename();
 
-/* ********** Auxiliar Methods ********** */
+        //TODO: Add all the errors
+        //Connection error
+        if (errorCode == 400) {
+
+        }
+        //Already owned file or the local file is the latest
+        else if (errorCode == 401) {
+            TransferWorker tw = requestsSent.remove(filename);
+            tw.closeSocket();
+        } else if (errorCode == 402) {
+            //Remover o ficheiro na maquina que recebe
+        } else if (errorCode == 403) {
+            //TODO: O que fazer aqui?
+        }
+    }
 
     /*
     Returns DatagramSocket with the first non-privileged available port.
     Returns null if there isnt an available port
      */
-    private DatagramSocket createDatagramSocket(){
+    private DatagramSocket createDatagramSocket() {
         boolean validPort = false;
         DatagramSocket ds = null;
-        for(int port = 1024; !validPort && port <= 32767 ; port++){
+        for (int port = 1024; !validPort && port <= 32767; port++) {
             try {
                 ds = new DatagramSocket(port);
                 validPort = true;
-            } catch (SocketException ignored) {}
+            } catch (SocketException ignored) {
+            }
         }
         return ds;
     }
 
     public void fillDirMap(String path) throws Exception {
         File dir = new File(path);
-        if (dir.isDirectory()){
+        if (dir.isDirectory()) {
             File[] fs = dir.listFiles();
             if (fs != null) {
                 for (File f : fs) {
@@ -217,7 +255,6 @@ public class ConnectionWorker extends Thread {
                     }
                 }
             }
-        }
-        else throw new Exception("Diretoria não encontrada");
+        } else throw new Exception("Diretoria não encontrada");
     }
 }
--- src/FFSync.java
+++ src/FFSync.java
@@ -1,7 +1,4 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.*;
 import java.nio.ByteBuffer;
 import java.util.*;
@@ -27,13 +24,12 @@ public class FFSync {
 
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         /*try {
             teste2();
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }*/
-
         String folderPath = args[0]; //Tem de acabar com a barra "/" no Linux ou com a barra "\" se for no Windows
         String externalIP = args[1];
         ReentrantLock readLock = new ReentrantLock();
@@ -43,13 +39,13 @@ public class FFSync {
         Map<String,Long> filesInDir;
         DatagramSocket ds;
 
+        //Connection verifications
         if (!testConnection(externalIP)) {
-            System.out.println("Não existe conexão a internet");
+            System.out.println("Couldn't find the other client!");
             return;
         }
 
         try {
-            filesInDir = fillDirMap(folderPath);
             ds = new DatagramSocket(9999);
             ds.connect(InetAddress.getByName(externalIP),9999);
             ds.setSoTimeout(10000); //10 seg de timeout
@@ -63,16 +59,13 @@ public class FFSync {
             return;
         }
 
-        ConnectionWorker receiver = new ConnectionWorker(true, externalIP, folderPath, filesInDir, ds, readLock, writeLock, requestsSent, requestsReceived);
-        ConnectionWorker sender   = new ConnectionWorker(false, externalIP, folderPath, filesInDir, ds, readLock, writeLock, requestsSent, requestsReceived);
-
         ///Authentication Block
-        FTrapid ft = new FTrapid(ds);
+        FTrapid ftr = new FTrapid(ds);
         Scanner sc = new Scanner(System.in);
         System.out.print("Introduza a sua password, em ambas as maquinas: ");
         String pass = sc.next();
         try {
-           if (ft.authentication(pass)!=1) {
+           if (ftr.authentication(pass)!=1) {
                System.out.println("Palavra passe errada");
                return;
            }
@@ -82,6 +75,18 @@ public class FFSync {
             return;
         }
         System.out.println("Sucesso na autenticação!!");
+
+        //Verifies the files that are required to send
+        if((filesInDir = fillDirMap(folderPath)) == null) {
+            System.out.println("Not a directory!");
+            return;
+        }
+        ftr.sendData(serialize((HashMap<String,Long>) filesInDir));
+        Map<String,Long> filesInDirReceived = deserialize(ftr.receiveData());
+        analyse(filesInDir,filesInDirReceived);
+
+        ConnectionWorker receiver = new ConnectionWorker(true, externalIP, folderPath, filesInDir, ds, readLock, writeLock, requestsSent, requestsReceived);
+        ConnectionWorker sender   = new ConnectionWorker(false, externalIP, folderPath, filesInDir, ds, readLock, writeLock, requestsSent, requestsReceived);
 
         receiver.start();
         sender.start();
@@ -113,7 +118,7 @@ public class FFSync {
 
     /* ******** Auxiliar Methods ******** */
 
-    public static Map<String,Long> fillDirMap(String path) throws Exception {
+    public static Map<String,Long> fillDirMap(String path){
         Map<String,Long> filesInDir = new HashMap<>();
         File dir = new File(path);
 
@@ -129,7 +134,7 @@ public class FFSync {
                 }
             }
         }
-        else throw new Exception("Diretoria não encontrada");
+        else return null;
 
         return filesInDir;
     }
@@ -142,6 +147,66 @@ public class FFSync {
             return true;
         }
         catch (IOException e) {/*System.out.println("Falhei!!!"); */return false;}
+    }
+
+    public static byte[] serialize(HashMap<String,Long> filesInDir) throws IOException {
+        byte[] bytes;
+        ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+        ObjectOutputStream out = new ObjectOutputStream(byteOut);
+
+        out.writeObject(filesInDir);
+        /*
+        out.writeInt(filesInDir.size());
+        for(Map.Entry<String,Long> entry : filesInDir.entrySet()) {
+            out.writeUTF(entry.getKey());
+            out.writeLong(entry.getValue());
+        }*/
+        bytes = byteOut.toByteArray();
+        out.close();
+        byteOut.close();
+
+        return bytes;
+    }
+/*
+    public static List<AbstractMap.SimpleEntry<String,Long>> deserialize(byte[] bytes) throws IOException, ClassNotFoundException {
+        List<AbstractMap.SimpleEntry<String,Long>> list = new ArrayList<>();
+        ByteArrayInputStream byteIn = new ByteArrayInputStream(bytes);
+        ObjectInputStream in = new ObjectInputStream(byteIn);
+
+        int nrOfFiles = in.readInt();
+
+        for(int i = 0; i < nrOfFiles; i++) list.add(new AbstractMap.SimpleEntry<String,Long>(in.readUTF(),in.readLong()) );
+
+        byteIn.close();
+        in.close();
+
+        return list;
+    }*/
+
+    public static Map<String,Long> deserialize(byte[] bytes) throws IOException, ClassNotFoundException {
+        Map<String,Long> map;
+        ByteArrayInputStream byteIn = new ByteArrayInputStream(bytes);
+        ObjectInputStream in = new ObjectInputStream(byteIn);
+
+        map = (HashMap<String,Long>) in.readObject();
+
+        //for(int i = 0; i < nrOfFiles; i++) list.add(new AbstractMap.SimpleEntry<String,Long>(in.readUTF(),in.readLong()) );
+
+        byteIn.close();
+        in.close();
+
+        return map;
+    }
+
+    public static void analyse(Map<String,Long> filesInDir, Map<String,Long> filesInDirReceived) {
+        String filename;
+
+        for(Map.Entry<String,Long> entry : filesInDirReceived.entrySet()) {
+            filename = entry.getKey();
+            //Checks for the existence of the file. If the file exists, compares the dates when they were last modified.
+            if (filesInDir.containsKey(filename) && filesInDir.get(filename) < entry.getValue())
+                filesInDir.remove(filename);
+        }
     }
 
     /* ******** Test Methods ******** */
